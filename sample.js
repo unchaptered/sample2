720ef6e3fd8cf9a3544cf5e3c76940264de51fce
unchaptered <workstation19961002@gmail.com>
2024-05-12 16:04:30 +0900

    Fix 코드

--- sample.js
+++ sample.js
@@ -1,3 +1,7 @@
 console.log('hello')
+            console.log('hello')
+            console.log('hello')
+            console.log('hello')
 console.log('hello')
-console.log('hello')
+            console.log('hello')
+        console.log('hello')
